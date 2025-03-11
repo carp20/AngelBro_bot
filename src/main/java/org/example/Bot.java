52dd5aa3f722904c6b1a5fdc97ddcbfe7dce37c0
carp20 <rijiymisha@gmail.com>
2025-03-11 21:08:05 +0300

    Add Gson

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -1,16 +1,16 @@
 package org.example;
 
+import com.google.gson.Gson;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.CopyMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Random;
-
-import static org.example.PokerCard.club;
 
 public class Bot extends TelegramLongPollingBot {
 
@@ -73,52 +73,125 @@ public class Bot extends TelegramLongPollingBot {
     }
     public void play(Long id){
         if(id==1966262220 || id==7519531395L || id==5809236702L) {
+            String txt;
             PokerCard[] pokerCard = PokerCard.values();
             String pokerCards = Arrays.toString(pokerCard);
+            int randomRank1 = random.nextInt(6, 15);
+            int iI1 = random.nextInt(0, 4);
+            String i1 = getRank(randomRank1);
 
-            for(int index = 0; index<2; index++) {
-                int randomRank = random.nextInt(6, 15);
-                int iI = random.nextInt(0, 4);
-                String i = null;
-
-                if (randomRank <= 10) {
-                    i = Integer.toString(randomRank);
-                }
-                else if(randomRank==11){
-                    i = "валет";
-                }
-                else if(randomRank==12){
-                    i = "дама";
-                }
-                else if(randomRank==13){
-                    i = "король";
-                }
-                else if(randomRank==14){
-                    i = "туз";
-                }
-
-                PokerGame identify = new PokerGame(pokerCard[iI], i);
-                if (identify.isClub) {
-                    sendText(id, "Поздравляем, тебе выпала карта треф " + i);
-                    System.out.println("Пользователю " + id + " выпала карта треф " + i);
-                } else if (identify.isDiamond) {
-                    sendText(id, "Поздравляем, тебе выпала карта бубен " + i);
-                    System.out.println("Пользователю " + id + " выпала карта бубен " + i);
-                } else if (identify.isHeart) {
-                    sendText(id, "Поздравляем, тебе выпала карта червь " + i);
-                    System.out.println("Пользователю " + id + " выпала карта червь " + i);
-                } else if (identify.isSpades) {
-                    sendText(id, "Поздравляем, тебе выпала карта пики " + i);
-                    System.out.println("Пользователю " + id + " выпала карта пики " + i);
-                } else {
-                    sendText(id, "Произошла какая-то ошибка! Повторите попытку позже!");
-                    System.out.println("Прошла ошибка");
-                }
+            PokerGame identify = new PokerGame(pokerCard[iI1], i1);
+            if (identify.isClub) {
+                sendText(id, "Поздравляем, тебе выпала карта треф " + i1);
+                txt = "Пользователю " + id + " выпала карта треф " + i1;
+                System.out.println(txt);
+                SaveGson(txt);
+            } else if (identify.isDiamond) {
+                sendText(id, "Поздравляем, тебе выпала карта бубен " + i1);
+                txt = "Пользователю " + id + " выпала карта бубен " + i1;
+                System.out.println(txt);
+                SaveGson(txt);
+            } else if (identify.isHeart) {
+                sendText(id, "Поздравляем, тебе выпала карта червь " + i1);
+                txt = "Пользователю " + id + " выпала карта червь " + i1;
+                System.out.println(txt);
+                SaveGson(txt);
+            } else if (identify.isSpades) {
+                sendText(id, "Поздравляем, тебе выпала карта пики " + i1);
+                txt = "Пользователю " + id + " выпала карта пики " + i1;
+                System.out.println(txt);
+                SaveGson(txt);
+            } else {
+                sendText(id, "Произошла какая-то ошибка! Повторите попытку позже!");
+                txt = "Прошла ошибка  пользователем " + id;
+                System.out.println(txt);
+                SaveGson(txt);
             }
+
+            int randomRank2 = random.nextInt(6, 15);
+            int iI2 = random.nextInt(0, 4);
+            String i2 = getRank(randomRank2);
+
+            PokerGame identify2 = new PokerGame(pokerCard[iI2], i2);
+            if (identify2.isClub) {
+                sendText(id, "Поздравляем, тебе выпала карта треф " + i2);
+                txt = "Пользователю " + id + " выпала карта треф " + i2;
+                System.out.println(txt);
+                SaveGson(txt);
+            } else if (identify2.isDiamond) {
+                sendText(id, "Поздравляем, тебе выпала карта бубен " + i2);
+                txt = "Пользователю " + id + " выпала карта бубен " + i2;
+                System.out.println(txt);
+                SaveGson(txt);
+            } else if (identify2.isHeart) {
+                sendText(id, "Поздравляем, тебе выпала карта червь " + i2);
+                txt = "Пользователю " + id + " выпала карта червь " + i2;
+                System.out.println(txt);
+                SaveGson(txt);
+            } else if (identify2.isSpades) {
+                sendText(id, "Поздравляем, тебе выпала карта пики " + i2);
+                txt = "Пользователю " + id + " выпала карта пики " + i2;
+                System.out.println(txt);
+                SaveGson(txt);
+            } else {
+                sendText(id, "Произошла какая-то ошибка! Повторите попытку позже!");
+                txt = "Прошла ошибка  пользователем " + id;
+                System.out.println(txt);
+                SaveGson(txt);
+            }
+
+            Combination(identify, identify2, id);
+
         }
         else {
             sendText(id,"У Вас нет прав использовать эту команду!");
             System.out.println("Пользователь " + id + " попытался использовать команду /play, но не смог (не тот id)");
+        }
+    }
+
+    public String getRank(int randomRank) {
+        String i = null;
+        if (randomRank <= 10) {
+            i = Integer.toString(randomRank);
+        }
+        else if (randomRank == 11) {
+            i = "валет";
+            }
+        else if (randomRank == 12) {
+            i = "дама";
+            }
+        else if (randomRank == 13) {
+            i = "король";
+            }
+        else if (randomRank == 14) {
+            i = "туз";
+            }
+        else {
+            return null;
+        }
+
+        return i;
+    }
+
+    public void Combination(PokerGame identify, PokerGame identify2, long id){
+        String a = identify.getRank();
+        String b = identify2.getRank();
+        String txt = "Пользователю " + id + " выпала пара карт " + b;
+        if(a.equals(b)){
+            sendText(id,"Поздравляем! Вам выпала пара " + a);
+            System.out.println(txt);
+        }
+    }
+
+    public void SaveGson(Object object){
+        Gson gson = new Gson();
+        String json = gson.toJson(object);
+        String filePath = "log.json";
+
+        try (FileWriter writer = new FileWriter(filePath)) {
+            writer.write(json);
+        } catch (IOException e) {
+            System.err.println("Ошибка записи в файл: " + e.getMessage());
         }
     }
 }
